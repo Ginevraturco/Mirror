90389f82dec06a4d43b23e10945ab3423f468124
Marco Secchi <loziosecchi@gmail.com>
2020-01-08 18:08:26 +0100

    Add Kenney assets.

---
+++ Assets/NABA/Scripts/RandomizePosition.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class RandomizePosition : NetworkBehaviour
+{
+    public override void OnStartLocalPlayer()
+    {
+        base.OnStartLocalPlayer();
+        base.OnStartAuthority();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(__RandomizePosition());
+    }
+
+    private IEnumerator __RandomizePosition()
+    {
+        WaitForSeconds wait = new WaitForSeconds(1f);
+        Vector3 startPosition = transform.position;
+
+        while (true)
+        {
+            transform.position += startPosition + ReturnRandom();
+            yield return wait;
+        }
+    }
+
+    private Vector3 ReturnRandom()
+    {
+        return new Vector3(
+            Random.Range(-4, 4),
+            Random.Range(-4, 4),
+            0
+        );
+
+    }
+}
---
+++ Assets/NABA/Scripts/ShipController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class ShipController : MonoBehaviour
+{
+    public float speed = 3;
+
+    public float rotationSpeed = 3;
+
+    private CharacterController _controller;
+
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float hMove = Input.GetAxis("Horizontal");
+        float vMove = Input.GetAxis("Vertical");
+
+        hMove = vMove > .1f ? hMove : 0;
+        transform.Rotate(0, hMove * rotationSpeed, 0);
+
+        vMove = Mathf.Clamp(vMove, 0, 1);
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        float currentSpeed = speed * vMove;
+        _controller.SimpleMove(forward * currentSpeed);
+    }
+}

266288797e5b690d2e182d8bc1269d724ba8928c
Marco Secchi <loziosecchi@gmail.com>
2020-01-13 11:36:24 +0100

    Add bullet spawning.

--- Assets/NABA/Scripts/ShipController_Networked.cs
+++ Assets/NABA/Scripts/ShipController_Networked.cs
@@ -10,11 +10,18 @@ public class ShipController_Networked : NetworkBehaviour
 
     public float rotationSpeed = 3;
 
+    public GameObject pointer;
+
+    public GameObject cannonballPrefab;
+
+    public Transform cannonballSpawnPoint;
+
     private CharacterController _controller;
 
     void Start()
     {
         _controller = GetComponent<CharacterController>();
+        pointer.SetActive(isLocalPlayer);
     }
 
     void Update()
@@ -29,5 +36,28 @@ public class ShipController_Networked : NetworkBehaviour
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         float currentSpeed = speed * vMove;
         _controller.SimpleMove(forward * currentSpeed);
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            CmdFire();
+        }
+
     }
+
+    [Command]
+    void CmdFire()
+    {
+        Debug.Log("[Server Message]: Firing bullet");
+        GameObject bullet = Instantiate(cannonballPrefab, cannonballSpawnPoint.position, transform.rotation);
+        NetworkServer.Spawn(bullet);
+        RpcOnFire();
+    }
+
+    [ClientRpc]
+    void RpcOnFire()
+    {
+        Debug.Log("[Client Message]: Bullet Fired");
+    }
+
+
 }

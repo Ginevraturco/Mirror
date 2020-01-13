d54a68f5088cd1c12279bc8c3fb95647097d0ab3
Marco Secchi <loziosecchi@gmail.com>
2020-01-13 11:00:02 +0100

    Add post process effects.

---
+++ Assets/NABA/Scripts/CameraController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using Mirror;
+using UnityEngine;
+
+[RequireComponent(typeof(CinemachineVirtualCamera))]
+public class CameraController : MonoBehaviour
+{
+    private CinemachineVirtualCamera _camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _camera = GetComponent<CinemachineVirtualCamera>();
+        StartCoroutine(nameof(FindPlayer));
+    }
+
+    // Update is called once per frame
+    private IEnumerator FindPlayer()
+    {
+        while (true)
+        {
+            GameObject[] list = GameObject.FindGameObjectsWithTag("Player");
+            foreach (GameObject player in list)
+            {
+                NetworkIdentity identity = player.GetComponent<NetworkIdentity>();
+                if (identity == null) continue;
+                if (identity.isLocalPlayer)
+                {
+                    _camera.LookAt = identity.transform;
+                    _camera.Follow = identity.transform;
+                    break;
+                }
+            }
+            yield return new WaitForSeconds(.1f);
+        }
+    }
+}
---
+++ Assets/NABA/Scripts/ShipController_Networked.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class ShipController_Networked : NetworkBehaviour
+{
+    public float speed = 3;
+
+    public float rotationSpeed = 3;
+
+    private CharacterController _controller;
+
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (!isLocalPlayer) return;
+        float hMove = Input.GetAxis("Horizontal");
+        float vMove = Input.GetAxis("Vertical");
+
+        transform.Rotate(0, hMove * rotationSpeed, 0);
+
+        vMove = Mathf.Clamp(vMove, 0, 1);
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        float currentSpeed = speed * vMove;
+        _controller.SimpleMove(forward * currentSpeed);
+    }
+}
